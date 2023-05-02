fa1fb2e00993bece6212b73465cd7fbc7ba2910d
Faria Afrose <85243808+faria-afrose@users.noreply.github.com>
2023-05-02 22:53:15 +0600

    Update data.js

--- js/data.js
+++ js/data.js
@@ -44,40 +44,236 @@ const data1 = [
 ];
 const data2 = [
   {
-    image: "./image/img/1.webp",
+    image: "./image/img/19.jpg",
     title: "32-pc Golden Egg Crunch Combo",
 
     para: "RM 24.99",
   },
   {
-    image: "./image/img/2.webp",
+    image: "./image/img/14.jpg",
 
     title: "36-pc Golden Egg Crunch Combo",
     para: "RM 24.99",
   },
   {
     title: "23-pc Golden Egg Crunch Combo",
-    image: "./image/img/3.webp",
+    image: "./image/img/17.jpg",
     para: "RM 24.99",
   },
   {
     title: "35-pc Golden Egg Crunch Combo",
-    image: "./image/img/4.webp",
+    image: "./image/img/18.jpg",
     para: "RM 24.99",
   },
   {
     title: "53-pc Golden Egg Crunch Combo",
-    image: "./image/img/5.webp",
+    image: "./image/img/25.webp ",
+    para: "RM 24.99",
+  },
+  {
+    title: "53-pc Golden Egg Crunch Combo",
+    image: "./image/img/26.jpg ",
     para: "RM 24.99",
   },
   {
     title: "33-pc Golden Egg Crunch Combo",
-    image: "./image/img/6.webp",
+    image: "./image/img/22.jpg",
     para: "RM 24.99",
   },
   {
     title: "13-pc Golden Egg Crunch Combo",
-    image: "./image/img/7.webp",
+    image: "./image/img/21.jpg",
     para: "RM 24.99",
+  },
+  {
+    title: "3-pc Golden Egg Crunch Combo",
+    image: "./image/img/6.webp",
+    para: "3RM 24.99",
+  },
+];
+const data1 = [
+  {
+    image: "./image/img/1.webp",
+    title: "3-pc Golden Egg Crunch Combo",
+
+    para: "1RM 24.99",
+  },
+  {
+    image: "./image/img/2.webp",
+
+    title: "3-pc Golden Egg Crunch Combo",
+    para: "2RM 24.99",
+  },
+  {
+    title: "3-pc Golden Egg Crunch Combo",
+    image: "./image/img/3.webp",
+    para: "3RM 24.99",
+  },
+  {
+    title: "3-pc Golden Egg Crunch Combo",
+    image: "./image/img/4.webp",
+    para: "4RM 24.99",
+  },
+  {
+    title: "3-pc Golden Egg Crunch Combo",
+    image: "./image/img/5.webp",
+    para: "5RM 24.99",
+  },
+  {
+    title: "3-pc Golden Egg Crunch Combo",
+    image: "./image/img/6.webp",
+    para: "6RM 24.99",
+  },
+  {
+    title: "3-pc Golden Egg Crunch Combo",
+    image: "./image/img/7.webp",
+    para: "7RM 24.99",
+  },
+  {
+    title: "3-pc Golden Egg Crunch Combo",
+    image: "./image/img/8.webp",
+    para: "8RM 24.99",
+  },
+];
+const data2 = [
+  {
+    image: "./image/img/19.jpg",
+    title: "32-pc Golden Egg Crunch Combo",
+
+    para: "RM 24.99",
+  },
+  {
+    image: "./image/img/14.jpg",
+
+    title: "36-pc Golden Egg Crunch Combo",
+    para: "RM 24.99",
+  },
+  {
+    title: "23-pc Golden Egg Crunch Combo",
+    image: "./image/img/17.jpg",
+    para: "RM 24.99",
+  },
+  {
+    title: "35-pc Golden Egg Crunch Combo",
+    image: "./image/img/18.jpg",
+    para: "RM 24.99",
+  },
+  {
+    title: "53-pc Golden Egg Crunch Combo",
+    image: "./image/img/25.webp ",
+    para: "RM 24.99",
+  },
+  {
+    title: "53-pc Golden Egg Crunch Combo",
+    image: "./image/img/26.jpg ",
+    para: "RM 24.99",
+  },
+  {
+    title: "33-pc Golden Egg Crunch Combo",
+    image: "./image/img/22.jpg",
+    para: "RM 24.99",
+  },
+  {
+    title: "13-pc Golden Egg Crunch Combo",
+    image: "./image/img/21.jpg",
+    para: "RM 24.99",
+  },
+  {
+    title: "3-pc Golden Egg Crunch Combo",
+    image: "./image/img/6.webp",
+    para: "3RM 24.99",
+  },
+];
+const data1 = [
+  {
+    image: "./image/img/1.webp",
+    title: "3-pc Golden Egg Crunch Combo",
+
+    para: "1RM 24.99",
+  },
+  {
+    image: "./image/img/2.webp",
+
+    title: "3-pc Golden Egg Crunch Combo",
+    para: "2RM 24.99",
+  },
+  {
+    title: "3-pc Golden Egg Crunch Combo",
+    image: "./image/img/3.webp",
+    para: "3RM 24.99",
+  },
+  {
+    title: "3-pc Golden Egg Crunch Combo",
+    image: "./image/img/4.webp",
+    para: "4RM 24.99",
+  },
+  {
+    title: "3-pc Golden Egg Crunch Combo",
+    image: "./image/img/5.webp",
+    para: "5RM 24.99",
+  },
+  {
+    title: "3-pc Golden Egg Crunch Combo",
+    image: "./image/img/6.webp",
+    para: "6RM 24.99",
+  },
+  {
+    title: "3-pc Golden Egg Crunch Combo",
+    image: "./image/img/7.webp",
+    para: "7RM 24.99",
+  },
+  {
+    title: "3-pc Golden Egg Crunch Combo",
+    image: "./image/img/8.webp",
+    para: "8RM 24.99",
+  },
+];
+const data2 = [
+  {
+    image: "./image/img/19.jpg",
+    title: "32-pc Golden Egg Crunch Combo",
+
+    para: "RM 24.99",
+  },
+  {
+    image: "./image/img/14.jpg",
+
+    title: "36-pc Golden Egg Crunch Combo",
+    para: "RM 24.99",
+  },
+  {
+    title: "23-pc Golden Egg Crunch Combo",
+    image: "./image/img/17.jpg",
+    para: "RM 24.99",
+  },
+  {
+    title: "35-pc Golden Egg Crunch Combo",
+    image: "./image/img/18.jpg",
+    para: "RM 24.99",
+  },
+  {
+    title: "53-pc Golden Egg Crunch Combo",
+    image: "./image/img/25.webp ",
+    para: "RM 24.99",
+  },
+  {
+    title: "53-pc Golden Egg Crunch Combo",
+    image: "./image/img/26.jpg ",
+    para: "RM 24.99",
+  },
+  {
+    title: "33-pc Golden Egg Crunch Combo",
+    image: "./image/img/22.jpg",
+    para: "RM 24.99",
+  },
+  {
+    title: "13-pc Golden Egg Crunch Combo",
+    image: "./image/img/21.jpg",
+    para: "RM 24.99",
+  },
+  {
+    title: "3-pc Golden Egg Crunch Combo",
+    image: "./image/img/6.webp",
+    para: "3RM 24.99",
   },
 ];
