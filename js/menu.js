3ae52677b0adaf04beee9599c5bc24b29f16fbc9
Faria Afrose <85243808+faria-afrose@users.noreply.github.com>
2023-05-23 23:50:15 +0600

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,708 @@
+window.onload = function () {
+  const data0 = [
+    {
+      id: "1",
+      image: "./image/img2/d9.jpg",
+      title: "Variety Box1",
+      para: "1RM 24.99",
+    },
+    {
+      id: "i21",
+      image: "./image/img2/c8.jpg",
+      title: "Variety Box1",
+      para: "1RM 24.99",
+    },
+    {
+      id: "i22",
+
+      image: "./image/img2/d7.jpg",
+
+      title: "Variety Box2",
+      para: "2RM 24.99",
+    },
+    {
+      id: "i22",
+
+      image: "./image/img2/v7.jpg",
+
+      title: "Variety Box2",
+      para: "2RM 24.99",
+    },
+    {
+      id: "i23",
+
+      title: "Variety Box3",
+      image: "./image/img2/n4.jpg",
+      para: "3RM 24.99",
+    },
+    {
+      id: "i24",
+
+      title: "Variety Box4",
+      image: "./image/img2/c0.jpg",
+      para: "4RM 24.99",
+    },
+    {
+      id: "i25",
+
+      title: "Variety Box5",
+      image: "./image/img2/d10.jpg",
+      para: "5RM 24.99",
+    },
+    {
+      id: "i26",
+
+      title: "Variety Box6",
+      image: "./image/img2/b1.jpg",
+      para: "6RM 24.99",
+    },
+    {
+      id: "i26",
+
+      title: "Variety Box6",
+      image: "./image/img2/drink2.jpg",
+      para: "6RM 24.99",
+    },
+  ];
+  const data3 = [
+    {
+      id: "i21",
+      image: "./image/img2/v5.jpg",
+      title: "Variety Box1",
+      para: "1RM 24.99",
+    },
+    {
+      id: "i22",
+
+      image: "./image/img2/v1.jpg",
+
+      title: "Variety Box2",
+      para: "2RM 24.99",
+    },
+    {
+      id: "i23",
+
+      title: "Variety Box3",
+      image: "./image/img2/v2.jpg",
+      para: "3RM 24.99",
+    },
+    {
+      id: "i24",
+
+      title: "Variety Box4",
+      image: "./image/img2/v3.jpg",
+      para: "4RM 24.99",
+    },
+    {
+      id: "i25",
+
+      title: "Variety Box5",
+      image: "./image/img2/v7.jpg",
+      para: "5RM 24.99",
+    },
+    {
+      id: "i26",
+
+      title: "Variety Box6",
+      image: "./image/img2/v6.jpg",
+      para: "6RM 24.99",
+    },
+  ];
+
+  const data4 = [
+    {
+      id: "i31",
+      image: "./image/img2/br3.jpg",
+      title: "Variety Box1",
+      para: "1RM 24.99",
+    },
+
+    {
+      id: "i33",
+
+      title: "Variety Box3",
+      image: "./image/img2/br12.jpg",
+      para: "3RM 24.99",
+    },
+    {
+      id: "i34",
+
+      title: "Variety Box4",
+      image: "./image/img2/br4.jpg",
+      para: "4RM 24.99",
+    },
+    {
+      id: "i35",
+
+      title: "Variety Box5",
+      image: "./image/img2/br5.jpg",
+      para: "5RM 24.99",
+    },
+    {
+      id: "i36",
+
+      title: "Variety Box6",
+      image: "./image/img2/br11.jpg",
+      para: "6RM 24.99",
+    },
+    {
+      id: "i37",
+
+      title: "Variety Box6",
+      image: "./image/img2/br1.jpg",
+      para: "6RM 24.99",
+    },
+  ];
+  const data5 = [
+    {
+      id: "i21",
+      image: "./image/img2/break1.jpg",
+      title: "Variety Box1",
+      para: "1RM 24.99",
+    },
+    {
+      id: "i22",
+
+      image: "./image/img2/break2.jpg",
+
+      title: "Variety Box2",
+      para: "2RM 24.99",
+    },
+    {
+      id: "i23",
+
+      title: "Variety Box3",
+      image: "./image/img2/bread1.jpg",
+      para: "3RM 24.99",
+    },
+    {
+      id: "i24",
+
+      title: "Variety Box4",
+      image: "./image/img2/break3.jpg",
+      para: "4RM 24.99",
+    },
+  ];
+
+  const data6 = [
+    {
+      id: "i31",
+      image: "./image/img2/c0.jpg",
+      title: "Variety Box1",
+      para: "1RM 24.99",
+    },
+
+    {
+      id: "i33",
+
+      title: "Variety Box3",
+      image: "./image/img2/c9.jpg",
+      para: "3RM 24.99",
+    },
+    {
+      id: "i34",
+
+      title: "Variety Box4",
+      image: "./image/img2/c7.jpg",
+      para: "4RM 24.99",
+    },
+    {
+      id: "i35",
+
+      title: "Variety Box5",
+      image: "./image/img2/c8.jpg",
+      para: "5RM 24.99",
+    },
+    {
+      id: "i36",
+
+      title: "Variety Box6",
+      image: "./image/img2/c5.jpg",
+      para: "6RM 24.99",
+    },
+    {
+      id: "i37",
+
+      title: "Variety Box6",
+      image: "./image/img2/c6.jpg",
+      para: "6RM 24.99",
+    },
+    {
+      id: "i38",
+
+      title: "Variety Box6",
+      image: "./image/img2/c2.jpg",
+      para: "6RM 24.99",
+    },
+    {
+      id: "i39",
+
+      title: "Variety Box6",
+      image: "./image/img2/c1.jpg",
+      para: "6RM 24.99",
+    },
+    {
+      id: "i40",
+
+      title: "Variety Box6",
+      image: "./image/img2/c4.jpg",
+      para: "6RM 24.99",
+    },
+  ];
+
+  const data7 = [
+    {
+      id: "i31",
+      image: "./image/img2/d1.jpg",
+      title: "Variety Box1",
+      para: "1RM 24.99",
+    },
+
+    {
+      id: "i37",
+
+      title: "Variety Box6",
+      image: "./image/img2/d6.jpg",
+      para: "6RM 24.99",
+    },
+    {
+      id: "i38",
+
+      title: "Variety Box6",
+      image: "./image/img2/d7.jpg",
+      para: "6RM 24.99",
+    },
+    {
+      id: "i39",
+
+      title: "Variety Box6",
+      image: "./image/img2/d8.jpg",
+      para: "6RM 24.99",
+    },
+    {
+      id: "i40",
+
+      title: "Variety Box6",
+      image: "./image/img2/d9.jpg",
+      para: "6RM 24.99",
+    },
+    {
+      id: "i41",
+
+      title: "Variety Box6",
+      image: "./image/img2/d10.jpg",
+      para: "6RM 24.99",
+    },
+    {
+      id: "i33",
+
+      title: "Variety Box3",
+      image: "./image/img2/d2.jpg",
+      para: "3RM 24.99",
+    },
+    {
+      id: "i34",
+
+      title: "Variety Box4",
+      image: "./image/img2/d3.jpg",
+      para: "4RM 24.99",
+    },
+    {
+      id: "i35",
+
+      title: "Variety Box5",
+      image: "./image/img2/d4.jpg",
+      para: "5RM 24.99",
+    },
+    {
+      id: "i36",
+
+      title: "Variety Box6",
+      image: "./image/img2/d5.jpg",
+      para: "6RM 24.99",
+    },
+  ];
+
+  const data8 = [
+    {
+      id: "i31",
+      image: "./image/img2/n1.jpg",
+      title: "Variety Box1",
+      para: "1RM 24.99",
+    },
+
+    {
+      id: "i33",
+
+      title: "Variety Box3",
+      image: "./image/img2/n2.jpg",
+      para: "3RM 24.99",
+    },
+    {
+      id: "i34",
+
+      title: "Variety Box4",
+      image: "./image/img2/n3.jpg",
+      para: "4RM 24.99",
+    },
+    {
+      id: "i35",
+
+      title: "Variety Box5",
+      image: "./image/img2/n4.jpg",
+      para: "5RM 24.99",
+    },
+  ];
+
+  const data9 = [
+    {
+      id: "i31",
+      image: "./image/img2/sf1.jpg",
+      title: "Variety Box1",
+      para: "1RM 24.99",
+    },
+
+    {
+      id: "i37",
+
+      title: "Variety Box6",
+      image: "./image/img2/sf2.jpg",
+      para: "6RM 24.99",
+    },
+    {
+      id: "i38",
+
+      title: "Variety Box6",
+      image: "./image/img2/sf3.jpg",
+      para: "6RM 24.99",
+    },
+    {
+      id: "i39",
+
+      title: "Variety Box6",
+      image: "./image/img2/sf4.jpg",
+      para: "6RM 24.99",
+    },
+    {
+      id: "i40",
+
+      title: "Variety Box6",
+      image: "./image/img2/sf5.jpg",
+      para: "6RM 24.99",
+    },
+  ];
+  const data10 = [
+    {
+      id: "i31",
+      image: "./image/img2/p4.jpg",
+      title: "Variety Box1",
+      para: "1RM 24.99",
+    },
+    {
+      id: "i31",
+      image: "./image/img2/p0.jpg",
+      title: "Variety Box1",
+      para: "1RM 24.99",
+    },
+
+    {
+      id: "i33",
+
+      title: "Variety Box3",
+      image: "./image/img2/p2.jpg",
+      para: "3RM 24.99",
+    },
+    {
+      id: "i34",
+
+      title: "Variety Box4",
+      image: "./image/img2/p3.jpg",
+      para: "4RM 24.99",
+    },
+  ];
+
+  const data11 = [
+    {
+      id: "i31",
+      image: "./image/img2/drink1.jpg",
+      title: "Variety Box1",
+      para: "1RM 24.99",
+    },
+
+    {
+      id: "i37",
+
+      title: "Variety Box6",
+      image: "./image/img2/drink2.jpg",
+      para: "6RM 24.99",
+    },
+    {
+      id: "i38",
+
+      title: "Variety Box6",
+      image: "./image/img2/drink3.jpg",
+      para: "6RM 24.99",
+    },
+    {
+      id: "i39",
+
+      title: "Variety Box6",
+      image: "./image/img2/drink4.jpg",
+      para: "6RM 24.99",
+    },
+    {
+      id: "i40",
+
+      title: "Variety Box6",
+      image: "./image/img2/drink5.jpg",
+      para: "6RM 24.99",
+    },
+    {
+      id: "i41",
+
+      title: "Variety Box6",
+      image: "./image/img2/drink6.jpg",
+      para: "6RM 24.99",
+    },
+    {
+      id: "i33",
+
+      title: "Variety Box3",
+      image: "./image/img2/drink7.jpg",
+      para: "3RM 24.99",
+    },
+    {
+      id: "i35",
+
+      title: "Variety Box5",
+      image: "./image/img2/j5.webp",
+      para: "5RM 24.99",
+    },
+    {
+      id: "i36",
+
+      title: "Variety Box6",
+      image: "./image/img2/j6.webp",
+      para: "6RM 24.99",
+    },
+    {
+      id: "i34",
+
+      title: "Variety Box4",
+      image: "./image/img2/drink10.jpg",
+      para: "4RM 24.99",
+    },
+    {
+      id: "i35",
+
+      title: "Variety Box5",
+      image: "./image/img2/j1.webp",
+      para: "5RM 24.99",
+    },
+    {
+      id: "i36",
+
+      title: "Variety Box6",
+      image: "./image/img2/j2.webp",
+      para: "6RM 24.99",
+    },
+    {
+      id: "i35",
+
+      title: "Variety Box5",
+      image: "./image/img2/j3.webp",
+      para: "5RM 24.99",
+    },
+    {
+      id: "i36",
+
+      title: "Variety Box6",
+      image: "./image/img2/j4.webp",
+      para: "6RM 24.99",
+    },
+  ];
+
+  const findG = document.getElementById("gallery");
+
+  function g(data) {
+    //gallery-item1
+    const gallery = document.createElement("div");
+    gallery.classList.add("gallery-item");
+    //content-div
+    const content = document.createElement("div");
+    content.classList.add("content");
+    //image-container
+    const main = document.createElement("div");
+
+    main.classList.add("image-container");
+    //btnH-h
+
+    const btnDiv = document.createElement("div");
+    btnDiv.classList.add("button_header");
+    const btnH = document.createElement("h1");
+    btnH.classList.add("header");
+    //   btnH.innerText = "hi";
+
+    const buttons = document.querySelectorAll("#box-btn button");
+    //tab-button list starts
+
+    const arrowR = document.querySelector(".wrapper .icon2 i");
+    const arrowL = document.querySelector(".wrapper .icon1 i");
+    const lContainer = document.querySelector(".wrapper .icon1 ");
+    const rContainer = document.querySelector(".wrapper .icon2 ");
+    const buttonList = document.querySelector(".wrapper .box-btn");
+
+    function funIconM() {
+      if (buttonList.scrollLeft >= 20) {
+        lContainer.classList.add("active");
+      } else {
+        lContainer.classList.remove("active");
+      }
+      let maxScrollValue = buttonList.scrollWidth - buttonList.clientWidth - 20;
+      console.log("scrollWidth", buttonList.scrollWidth);
+      console.log(maxScrollValue);
+      console.log("clientWidth", buttonList.clientWidth);
+      console.log("scrollLeft", buttonList.scrollLeft);
+      if (buttonList.scrollLeft >= maxScrollValue) {
+        rContainer.classList.remove("active");
+      } else {
+        rContainer.classList.add("active");
+      }
+    }
+
+    arrowR.addEventListener("click", () => {
+      buttonList.scrollLeft += 200;
+      console.log("you chick");
+      funIconM();
+    });
+    arrowL.addEventListener("click", () => {
+      buttonList.scrollLeft -= 200;
+      console.log("you chick");
+      funIconM();
+    });
+    buttonList.addEventListener("scroll", funIconM);
+    //tab-button list ends
+    // buttons.forEach(function (button) {
+    //   for (var i = 0; i < buttons.length; i++) {
+    //     var buttonText = buttons[i].textContent;
+    //     btnH.textContent = buttonText;
+    //   }
+    //   button.addEventListener("click", function () {
+    //     buttons.forEach(function (btn) {
+    //       btn.classList.remove("active");
+    //     });
+    //     button.classList.add("active");
+    //   });
+    // });
+
+    data.forEach((item) => {
+      const GItem = document.createElement("div");
+
+      GItem.setAttribute("data-model-target", "#model");
+      GItem.classList.add("item");
+
+      GItem.addEventListener("click", function (e) {
+        const target = e.target;
+        openModule(model);
+
+        console.log(e.target);
+      });
+      //img
+      const image = document.createElement("img");
+      image.src = item.image;
+
+      //img-h
+      const header = document.createElement("h2");
+      header.innerText = item.title;
+      //img-p
+
+      const para = document.createElement("p");
+      para.textContent = item.para;
+      //for badge start
+      const badge = document.createElement("p");
+
+      badge.innerHTML = `<span class="badge">New!</span>`;
+
+      //for badge ends
+      //new parts
+
+      //new parts
+
+      //btn
+
+      buttons.forEach(function (button) {
+        for (var i = 0; i < buttons.length; i++) {
+          var buttonText = buttons[i].textContent;
+          btnH.textContent = buttonText;
+        }
+        button.addEventListener("click", function (e) {
+          buttons.forEach(function (btn) {
+            btn.classList.remove("active");
+          });
+          button.classList.add("active");
+
+          //tab indicator part
+          var tIndicator = document.querySelector(".tab-indicator");
+          tIndicator.style.width = e.target.offsetWidth + "px";
+          tIndicator.style.left = e.target.offsetLeft + "px";
+          //ends tab indicator part
+
+          //new parts
+
+          //new parts
+        });
+      });
+
+      //btn
+
+      GItem.appendChild(image);
+      GItem.appendChild(header);
+      GItem.appendChild(para);
+      GItem.appendChild(badge);
+
+      main.appendChild(GItem);
+      gallery.appendChild(main);
+      btnDiv.appendChild(btnH);
+      content.appendChild(btnDiv);
+      content.appendChild(gallery);
+    });
+
+    return content;
+  }
+
+  //data2-burger
+
+  const g2 = g(data4);
+  findG.appendChild(g2);
+  //data6-pasta
+
+  const g6 = g(data8);
+  findG.appendChild(g6);
+  //data7-seafood
+  const g7 = g(data9);
+  findG.appendChild(g7);
+
+  //data4-shrimp
+  const g4 = g(data6);
+  findG.appendChild(g4);
+
+  //data5-rice
+  const g5 = g(data7);
+  findG.appendChild(g5);
+  //data3-breakfast
+
+  const g3 = g(data5);
+  findG.appendChild(g3);
+  //data1-salad
+  const g1 = g(data3);
+  findG.appendChild(g1);
+  //data8-pizza
+  const g8 = g(data10);
+  findG.appendChild(g8);
+  //data0-family
+  const g0 = g(data0);
+  findG.appendChild(g0);
+  //data9-drink
+  const g9 = g(data11);
+  findG.appendChild(g9);
+};
